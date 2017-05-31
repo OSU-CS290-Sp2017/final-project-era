5e4d02dcbf9952b592e20074149d421b42ccfff8
helq2612 <heli@oregonstate.edu>
2017-05-30 21:21:06 -0700

    change by heli, change orderDrink.js

--- js/orderDrink.js
+++ js/orderDrink.js
@@ -1,6 +1,22 @@
-// var drinkSelectRadioButtons = document.getElementsByClassName('drinks-size');
+var drinkSelectRadioButtons = document.getElementsByClassName('drinks-size');
+
+var testButton = document.getElementById('hotDrink-coffee-12');
+function test(event){
+	console.log("event"+event);
+}
+testButton.addEventListener('click', test);
+
+
 // for(drinkSelectRadioButton in drinkSelectRadioButtons){
-// 	drinkSelectRadioButton.onClick = function(){
-// 		alert(drinkSelectRadioButtons.value);
-// 	};
+
+// 	// drinkSelectRadioButtons[drinkSelectRadioButtons].addEventListener('click', test);
+// 	console.log("buttone =="+drinkSelectRadioButton);
 // }
+
+for (var i = 0; i < drinkSelectRadioButtons.length; i++) {
+	// console.log(drinkSelectRadioButtons[i]);
+	drinkSelectRadioButtons[i].addEventListener('click', function(event){
+		console.log("event"+event+drinkSelectRadioButtons[i]);
+	},false);
+	// console.log("buttone =="+drinkSelectRadioButton);
+}
